3e0ea7f3c2150473dffa330f44ee12275960c08d
Roman Dzieciol <roman.dzieciol@gmail.com>
2019-06-29 00:12:22 +0100

    swift-syntax-util 0.0.2

--- Package.swift
+++ Package.swift
@@ -19,7 +19,7 @@ let package = Package(
         // Dependencies declare other packages that this package depends on.
         // .package(url: /* package url */, from: "1.0.0"),
         .package(url: "https://github.com/apple/swift-syntax.git", .exact("0.50000.0")),
-        .package(path: "../swift-syntax-util")
+        .package(url: "https://github.com/roman-dzieciol/swift-syntax-util.git", .exact("0.0.2"))
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
